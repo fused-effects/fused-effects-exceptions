39ca4b9b155bf02f36bc9ed0666220c6260dd31c
Rob Rix <rob.rix@me.com>
2020-07-13 11:15:51 -0400

    Placate hlint.

--- test/Main.hs
+++ test/Main.hs
@@ -19,12 +19,12 @@ problematic =
 
 testStateDropsWrites :: Tasty.TestTree
 testStateDropsWrites = HUnit.testCase "State.Strict drops writes" $ do
-  result <- State.execState 'a' $ problematic
+  result <- State.execState 'a' problematic
   result HUnit.@?= 'a' -- writes are lost
 
 testIOStatePreservesWrites :: Tasty.TestTree
 testIOStatePreservesWrites = HUnit.testCase "State.IORef preserves writes" $ do
-  result <- IOState.execState 'a' $ problematic
+  result <- IOState.execState 'a' problematic
   result HUnit.@?= 'x'
 
 tests :: Tasty.TestTree
